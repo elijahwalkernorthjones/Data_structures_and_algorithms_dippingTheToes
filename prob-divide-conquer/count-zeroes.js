b62755403c05c6b670cf3a7ddbfca47010aea695
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2023-11-20 18:29:08 -0500

    a couple more problems down

--- prob-divide-conquer/count-zeroes.js
+++ prob-divide-conquer/count-zeroes.js
@@ -1,5 +1,22 @@
-function countZeroes() {
-  
+function countZeros(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        // Check if the middle element is 1
+        if (arr[mid] === 1) {
+            // Move to the right half
+            left = mid + 1;
+        } else {
+            // Move to the left half (including the current mid)
+            right = mid;
+        }
+    }
+
+    // The 'left' pointer now points to the first occurrence of 1 meeting 0
+    return arr.length - left;
 }
 
-module.exports = countZeroes
+// module.exports = countZeroes
--- prob-divide-conquer/find-rotated-index.js
+++ prob-divide-conquer/find-rotated-index.js
@@ -1,5 +1,34 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, target) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if (middleVal === target) {
+            return middleIdx;
+        }
+
+        if (arr[leftIdx] <= middleVal) {
+            // Left half is sorted
+            if (target >= arr[leftIdx] && target < middleVal) {
+                rightIdx = middleIdx - 1;
+            } else {
+                leftIdx = middleIdx + 1;
+            }
+        } else {
+            // Right half is sorted
+            if (target > middleVal && target <= arr[rightIdx]) {
+                leftIdx = middleIdx + 1;
+            } else {
+                rightIdx = middleIdx - 1;
+            }
+        }
+    }
+
+    return -1;
 }
 
-module.exports = findRotatedIndex
+// module.exports = findRotatedIndex
+// expect(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8)).toBe(2)
